2434be43b9272d547b4c02148d4906a7aab2c8de
Abhijit2714 <abijitdhamne@gmail.com>
2024-01-12 14:29:24 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
-def call(String name, String name) {
-    sh "Name of employee is ${name}, and organization name is ${name}."
+def call(String name, String dayOfweek) {
+    sh "Name of employee is ${name}, and organization name is ${dayOfweek}."
 }
