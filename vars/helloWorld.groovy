1857f259126085b614aee12b98fa39cdd1d4c59d
Abhijit2714 <abijitdhamne@gmail.com>
2024-01-12 14:43:52 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
 def call(Map config = [:]) {
-    sh "echo Employee name is Abhijit Dhamne. Organization is Infivit Technologies"
+    sh "Employee name is Abhijit Dhamne. Organization is Infivit Technologies"
 }
